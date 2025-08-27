716cd0d246e5cf63d016d3464373f73a7b8f1bfd
faisale <fa6769@gmaile.com>
2025-08-27 18:27:39 +0600

    running call daligation

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ function getElement(id){
 
 document.getElementById("card-box").addEventListener('click',function(e){
     e.preventDefault()
+
+    
+    
     if (e.target.id === "love") {
         let loveCount = Number(getElement("love-count").innerText);
         loveCount++;
         getElement("love-count").innerText = loveCount;
     }
+    
     if (e.target.id === "call") {
+
+        const buttonParent = e.target.parentNode;
+        const card = buttonParent.parentNode; 
+        const ServiceName = card.querySelector(".Service-name").innerText;
+        const ServiceNumber = card.querySelector(".Service-number").innerText;
+        alert(`${ServiceNumber} - ${ServiceName }`)
+
+
+      
         const mainCoinCount = Number(getElement("coins").innerText);
         if (mainCoinCount <20) {
-            alert("you dont have enough coins to make this call!")
+            alert("You don't have enough coins to make this call!")
             return;
         }
         let coinCount = Number(getElement("coins").innerText);
