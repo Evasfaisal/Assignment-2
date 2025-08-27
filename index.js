ee27f2763a3b6077b40aa41c6d284881fee650ac
faisale <fa6769@gmaile.com>
2025-08-27 22:45:43 +0600

    onek hard chilo

--- index.js
+++ index.js
@@ -16,14 +16,6 @@ document.getElementById("card-box").addEventListener('click',function(e){
     
     if (e.target.id === "call") {
 
-        const buttonParent = e.target.parentNode;
-        const card = buttonParent.parentNode; 
-        const ServiceName = card.querySelector(".Service-name").innerText;
-        const ServiceNumber = card.querySelector(".Service-number").innerText;
-        alert(`${ServiceNumber} - ${ServiceName }`)
-
-
-      
         const mainCoinCount = Number(getElement("coins").innerText);
         if (mainCoinCount <20) {
             alert("You don't have enough coins to make this call!")
@@ -32,6 +24,34 @@ document.getElementById("card-box").addEventListener('click',function(e){
         let coinCount = Number(getElement("coins").innerText);
         coinCount -=20;
         getElement("coins").innerText = coinCount;
+
+
+
+        const buttonParent = e.target.parentNode;
+        const card = buttonParent.parentNode; 
+        const ServiceName = card.querySelector(".Service-name").innerText;
+        const ServiceNumber = card.querySelector(".Service-number").innerText;
+        alert(`${ServiceName}-${ServiceNumber}`)
+
+
+
+        const historyContainer = getElement("History")
+
+        const createDiv = document.createElement("div")
+        createDiv.innerHTML = `   <div
+                        class="History-child border rounded-lg w-[352px] h-[105px]  p-5 text-sm bg-gray-100 mt-5 flex flex-col justify-between">
+                        <div class="flex justify-between items-center ">
+                            <h1 class="font-bold">${ServiceName}</h1>
+
+                            <span class="call-time">${new Date().toLocaleTimeString()}</span>
+                        </div>
+                        <p>${ServiceNumber}</p>
+                    </div>
+        `
+        historyContainer.appendChild(createDiv);
+
+        
+      
     }
 
 })
